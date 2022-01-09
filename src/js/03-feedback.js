637c973997124564ce3c75b0ee3229dd1ef8ced6
Bomberman93 <nikita.bondarenko.1993@gmail.com>
2022-01-09 23:48:04 +0200

    dz8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,5 @@
-// Выполняй это задание в файлах 03-feedback.html и 03-feedback.js. Разбей его на несколько подзадач:
 
-// Отслеживай на форме событие input, и каждый раз записывай в локальное хранилище объект с полями email и password, в которых сохраняй текущие значения полей формы. Пусть ключом для хранилища будет строка "feedback-form-state".
-// При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные данные, заполняй ими поля формы. В противном случае поля должны быть пустыми.
-// При сабмите формы очищай хранилище и поля формы, а также выводи объект с полями email, password и текущими их значениями в консоль.
-// Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. Для этого добавь в проект и используй библиотеку lodash.throttle.
+import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector(".feedback-form");
 const inputEl = document.querySelector("input");
@@ -12,10 +8,34 @@ const textarea = document.querySelector("textarea");
 const FEED = "feedback-form-state";
 const localValue = localStorage.getItem(FEED);
 
+formEl.addEventListener("input", throttle(formEmail,500));
+// textarea.addEventListener("input", formMessage);
+
+function formEmail (event) {
+    event.preventDefault();
+    const form = {
+        email: inputEl.value,
+        message: textarea.value, 
+    };
+    localStorage.setItem(FEED, JSON.stringify(form))
+};
+
 function saveLocal () {
-    if(JSON.parse(localValue)){
+    if(localValue){
         inputEl.value = JSON.parse(localValue).email;
         textarea.value = JSON.parse(localValue).message;
     }
 }
-saveLocal ();
+saveLocal();
+
+formEl.addEventListener("submit", submitButton);
+
+function submitButton (event) {
+    event.preventDefault();
+    console.log({
+        email: inputEl.value,
+        message: textarea.value, 
+    });
+    formEl.reset();
+    localStorage.removeItem(FEED);
+}
